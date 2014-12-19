a8b97182dfd52cc746686a41af7031b2491aaf6b
Bernd Ahlers <bernd@tuneafish.de>
2014-12-19 13:24:53 +0100

    Add message compression test and implement Snappy codec.

--- src/main/java/com/github/bernd/samsa/compression/CompressionFactory.java
+++ src/main/java/com/github/bernd/samsa/compression/CompressionFactory.java
@@ -1,30 +1,42 @@
 package com.github.bernd.samsa.compression;
 
+import org.xerial.snappy.SnappyInputStream;
+import org.xerial.snappy.SnappyOutputStream;
+
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 
 public class CompressionFactory {
-    public static OutputStream create(final CompressionCodec compressionCodec, final OutputStream stream) {
+    public static OutputStream create(final CompressionCodec compressionCodec, final OutputStream stream) throws IOException {
         switch (compressionCodec) {
             case NONE:
                 return stream;
             case GZIP:
-                // return new GZIPOutputStream(stream);
+                return new GZIPOutputStream(stream);
             case SNAPPY:
+                return new SnappyOutputStream(stream);
             case LZ4:
+                // TODO Implement KafkaLZ4BlockOutputStream
+                //return new KafkaLZ4BlockOutputStream(stream);
             default:
                 throw new UnknownCodecException("Unknown Codec: " + compressionCodec);
         }
     }
 
-    public static InputStream create(final CompressionCodec compressionCodec, final InputStream stream) {
+    public static InputStream create(final CompressionCodec compressionCodec, final InputStream stream) throws IOException {
         switch (compressionCodec) {
             case NONE:
                 return stream;
             case GZIP:
-                // return new GZIPInputStream(stream);
+                return new GZIPInputStream(stream);
             case SNAPPY:
+                return new SnappyInputStream(stream);
             case LZ4:
+                // TODO Implement KafkaLZ4BlockInputStream
+                //return new new KafkaLZ4BlockInputStream(stream);
             default:
                 throw new UnknownCodecException("Unknown Codec: " + compressionCodec);
         }
---
+++ src/test/java/com/github/bernd/samsa/message/MessageCompressionTest.java
@@ -0,0 +1,65 @@
+package com.github.bernd.samsa.message;
+
+import com.github.bernd.samsa.compression.CompressionCodec;
+import com.google.common.collect.Lists;
+import org.testng.annotations.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.util.List;
+
+import static org.testng.Assert.assertEquals;
+
+public class MessageCompressionTest {
+    @Test
+    public void testSimpleCompressDecompress() throws Exception {
+        final List<CompressionCodec> codecs = Lists.newArrayList(CompressionCodec.GZIP);
+
+        if(isSnappyAvailable()) {
+            codecs.add(CompressionCodec.SNAPPY);
+        }
+
+        // TODO Implement LZ4 compression/decompression.
+        /*
+        if(isLZ4Available()) {
+            codecs.add(CompressionCodec.LZ4);
+        }
+        */
+
+        for(final CompressionCodec codec : codecs) {
+            testSimpleCompressDecompress(codec);
+        }
+    }
+
+    public void testSimpleCompressDecompress(final CompressionCodec compressionCodec) throws Exception {
+        final List<Message> messages = Lists.newArrayList(
+                new Message("hi there".getBytes()),
+                new Message("I am fine".getBytes()),
+                new Message("I am not so well today".getBytes())
+        );
+        final ByteBufferMessageSet messageSet = new ByteBufferMessageSet(compressionCodec, messages);
+        assertEquals(messageSet.shallowIterator().next().getMessage().compressionCodec(), compressionCodec);
+        final List<Message> decompressed = Lists.newArrayList();
+        for (MessageAndOffset aMessageSet : messageSet) {
+            decompressed.add(aMessageSet.getMessage());
+        }
+        assertEquals(messages, decompressed);
+    }
+
+    private boolean isSnappyAvailable() {
+        try {
+            final org.xerial.snappy.SnappyOutputStream stream = new org.xerial.snappy.SnappyOutputStream(new ByteArrayOutputStream());
+            return true;
+        } catch (UnsatisfiedLinkError | org.xerial.snappy.SnappyError e) {
+            return false;
+        }
+    }
+
+    private boolean isLZ4Available() {
+        try {
+            final net.jpountz.lz4.LZ4BlockOutputStream stream = new net.jpountz.lz4.LZ4BlockOutputStream(new ByteArrayOutputStream());
+            return true;
+        } catch (UnsatisfiedLinkError e) {
+            return false;
+        }
+    }
+}
