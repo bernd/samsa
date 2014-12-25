8b1fc59ff9a8dd7838c0b25065653260dc0c8655
Bernd Ahlers <bernd@tuneafish.de>
2014-12-25 13:35:57 +0100

    Simpler example.

--- src/test/java/com/github/bernd/samsa/executable/WriteReadTest.java
+++ src/test/java/com/github/bernd/samsa/executable/WriteReadTest.java
@@ -8,6 +8,7 @@ import com.github.bernd.samsa.LogConfig;
 import com.github.bernd.samsa.LogConfigBuilder;
 import com.github.bernd.samsa.LogManager;
 import com.github.bernd.samsa.TopicAndPartition;
+import com.github.bernd.samsa.cleaner.CleanerConfig;
 import com.github.bernd.samsa.cleaner.CleanerConfigBuilder;
 import com.github.bernd.samsa.compression.CompressionCodec;
 import com.github.bernd.samsa.message.ByteBufferMessageSet;
@@ -19,32 +20,33 @@ import com.github.bernd.samsa.utils.SystemTime;
 import com.github.bernd.samsa.utils.Utils;
 import com.google.common.base.Optional;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class WriteReadTest {
     private static final Logger LOG = LoggerFactory.getLogger(WriteReadTest.class);
 
     public static void main(String[] args) throws Throwable {
+        final ArrayList<File> logDirs = Lists.newArrayList(new File("/tmp/samsa-test"));
+        final Map<String, LogConfig> topicConfigs = Maps.newHashMap();
+        final LogConfig logConfig = new LogConfigBuilder().build();
+        final CleanerConfig cleanerConfig = new CleanerConfigBuilder().build();
         final int ioThreads = 2;
         final long flushCheckMs = TimeUnit.SECONDS.toMillis(60);
         final long flushCheckpointMs = TimeUnit.SECONDS.toMillis(60);
         final long retentionCheckMs = TimeUnit.SECONDS.toDays(20);
+        final SamsaScheduler scheduler = new SamsaScheduler(2);
+        final BrokerState brokerState = new BrokerState();
+        final SystemTime time = new SystemTime();
 
-        final LogConfig logConfig = new LogConfigBuilder().build();
-
-        final LogManager logManager = new LogManager(Lists.newArrayList(new File("/tmp/samsa-test")),
-                new HashMap<String, LogConfig>(),
-                logConfig,
-                new CleanerConfigBuilder().build(),
-                ioThreads, flushCheckMs, flushCheckpointMs, retentionCheckMs,
-                new SamsaScheduler(2),
-                new BrokerState(),
-                new SystemTime());
+        final LogManager logManager = new LogManager(logDirs, topicConfigs, logConfig, cleanerConfig, ioThreads,
+                flushCheckMs, flushCheckpointMs, retentionCheckMs, scheduler, brokerState, time);
 
         final TopicAndPartition topicAndPartition = new TopicAndPartition("test", 0);
         final Optional<Log> logOptional = logManager.getLog(topicAndPartition);
@@ -71,5 +73,9 @@ public class WriteReadTest {
 
             LOG.info("Read message: \"{}\" (at {})", new String(Utils.readBytes(message.payload())), offset);
         }
+
+        log.flush();
+        log.close();
+        logManager.shutdown();
     }
 }
